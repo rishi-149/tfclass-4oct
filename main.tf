0d7e41e906758face037d844e1612d5223f5afa6
Rishi Gupta <rishigupta1492000@gmail.com>
2026-05-06 12:54:24 +0530

    create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-s3-bucket-${terraform workspace}"
+  acl = "private"
+
+  tags = {
+    name = "MyBucket-${terraform.workspace}"
+    Environment = terraform.workspace
+  }
+}
