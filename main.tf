7e40745bb8ed428809622ca396af21506feb0fa4
Rishi Gupta <rishigupta1492000@gmail.com>
2026-05-06 13:47:12 +0530

    update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-s3-bucket-${terraform workspace}"
+  bucket = "my-s3-bucket-${terraform.workspace}"
   acl = "private"
 
   tags = {
